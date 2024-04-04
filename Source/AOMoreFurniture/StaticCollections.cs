5584ecada474f767df4568f0670a62aecc7e4bfd
SokyranTheDragon <exploder1531@gmail.com>
2024-04-04 02:25:06 +0200

    Modify WorkGiver_CleanBin to use a list of defs with bin cleaning comp
    Changes introduced in this PR:
    - Added `StaticCollections` class, which hold a list of all `ThingDef`s which have `CompBinClean`
    - `WorkGiver_CleanBin.PotentialWorkThingsGlobal` will now use that list
      - Compared to previous implementation, bins added by other mods will be supported
      - It doesn't require a lookup to `DefDatabase`

---
+++ Source/AOMoreFurniture/StaticCollections.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Verse;
+
+namespace VanillaFurnitureEC
+{
+    [StaticConstructorOnStartup]
+    public static class StaticCollections
+    {
+        public static List<ThingDef> compBinCleanDefs = DefDatabase<ThingDef>.AllDefsListForReading
+            .Where(def => def.comps.Any(comp => comp is CompProperties_BinClean)).ToList();
+    }
+}
