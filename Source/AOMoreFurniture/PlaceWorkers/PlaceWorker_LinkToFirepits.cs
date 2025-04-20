bed6d6445eea56eb4cddb0cc80d927358b0de83b
SokyranTheDragon <exploder1531@gmail.com>
2025-04-20 17:45:38 +0200

    Update place worker
    - It uses constant range, since there was no place to ever change it
    - It will use the built-in vanilla campfire radius (if it has one) or default to 11.9 (which is now constant)
    - It will no longer draw its own radius ring, unless there's no vanilla place worker

--- Source/AOMoreFurniture/PlaceWorkers/PlaceWorker_LinkToFirepits.cs
+++ Source/AOMoreFurniture/PlaceWorkers/PlaceWorker_LinkToFirepits.cs
@@ -8,13 +8,24 @@ namespace VanillaFurnitureEC;
 public class PlaceWorker_LinkToFirepits : PlaceWorker
 {
     private static readonly ThingDef[] Firepits = new[] {VFE_DefOf.Stone_Campfire, VFE_DefOf.Stone_DarklightCampfire}.Where(x => x != null).ToArray();
-    public float range = 11.9f;
+    public const float Range = 11.9f;
 
     public override void DrawGhost(ThingDef def, IntVec3 center, Rot4 rot, Color ghostCol, Thing thing = null)
     {
-        GenDraw.DrawRadiusRing(center, range);
         foreach (var firepit in Firepits)
         {
+            var placeWorker = firepit.PlaceWorkers.OfType<PlaceWorker_LinkToCampfire>().FirstOrDefault();
+            float range;
+            if (placeWorker == null)
+            {
+                range = Range;
+                GenDraw.DrawRadiusRing(center, range);
+            }
+            else
+            {
+                range = placeWorker.range;
+            }
+
             var forCell = Find.CurrentMap.listerBuldingOfDefInProximity.GetForCell(center, range, firepit);
             for (var i = 0; i < forCell.Count; i++)
                 GenDraw.DrawLineBetween(GenThing.TrueCenter(center, Rot4.North, def.size, def.Altitude), forCell[i].TrueCenter(), SimpleColor.Green);
