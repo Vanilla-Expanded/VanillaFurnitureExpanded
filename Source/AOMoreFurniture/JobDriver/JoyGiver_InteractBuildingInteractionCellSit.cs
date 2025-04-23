e5103029f83291c8407d51bb9f2ed447056c4748
SokyranTheDragon <exploder1531@gmail.com>
2025-04-23 21:19:02 +0200

    Remove some more testing code

--- Source/AOMoreFurniture/JobDriver/JoyGiver_InteractBuildingInteractionCellSit.cs
+++ Source/AOMoreFurniture/JobDriver/JoyGiver_InteractBuildingInteractionCellSit.cs
@@ -6,15 +6,6 @@ namespace VanillaFurnitureEC;
 
 public class JoyGiver_InteractBuildingInteractionCellSit : JoyGiver_InteractBuildingInteractionCell
 {
-    public override float GetChance(Pawn pawn)
-    {
-        var chance = base.GetChance(pawn);
-        if (pawn.needs?.rest?.CurLevelPercentage <= 0.3f)
-            chance *= 2f;
-
-        return chance;
-    }
-
     protected override Job TryGivePlayJob(Pawn pawn, Thing thing)
     {
         // If the JoyGiver requires a chair, include an interaction cell chair check
