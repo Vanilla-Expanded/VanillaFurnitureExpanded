1f5f424fa57a836dad237cf8f1d6b31cf3b99123
SokyranTheDragon <exploder1531@gmail.com>
2025-04-26 01:33:50 +0200

    Improvements to desk and floor lamps
    - They are no longer edifices
    - They have proper layers for rendering
      - Desk lamp got `BuildingOnTop`, as it seemed fitting
      - Floor lamp got `Conduits` layer
        - We may consider something different, like `FloorCoverings`? (construction would conflict with Ideology slabs)
    - They have `clearBuildingArea` set to false so they can be built on top of/under buildings (without having them deconstructed first)
    - Made a place worker for them so they can't be placed on walls (anything with fillPercent of 1)

---
+++ Source/AOMoreFurniture/PlaceWorkers/PlaceWorker_NotOnWall.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaFurnitureEC;
+
+public class PlaceWorker_NotOnWall : PlaceWorker
+{
+    public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+    {
+        var list = loc.GetThingList(map);
+        for (var i = 0; i < list.Count; i++)
+        {
+            if (GenConstruct.BuiltDefOf(list[i].def) is ThingDef { Fillage: FillCategory.Full })
+                return "VFE.CannotPlaceOnWall".Translate();
+        }
+
+        return true;
+    }
+}
