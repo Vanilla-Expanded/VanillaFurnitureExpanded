a75dc7a25cb57bda9412088f92e415976097f221
SokyranTheDragon <exploder1531@gmail.com>
2025-05-11 17:10:20 +0200

    Add line of sight check to darts board

---
+++ Source/AOMoreFurniture/JobDriver/JoyGiver_InteractBuildingInteractionCell_LineOfSightCheck.cs
@@ -0,0 +1,15 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaFurnitureEC;
+
+public class JoyGiver_InteractBuildingInteractionCell_LineOfSightCheck : JoyGiver_InteractBuildingInteractionCell
+{
+    protected override bool CanInteractWith(Pawn pawn, Thing t, bool inBed)
+    {
+        if (!NonPublicMethods.WatchBuildingUtility_EverPossibleToWatchFrom(t.InteractionCell, t.Position, t.Map, false, t.def))
+            return false;
+
+        return base.CanInteractWith(pawn, t, inBed);
+    }
+}
---
+++ Source/AOMoreFurniture/NonPublicMethods.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaFurnitureEC;
+
+[StaticConstructorOnStartup]
+public static class NonPublicMethods
+{
+    public static EverPossibleToWatchFromDelegate WatchBuildingUtility_EverPossibleToWatchFrom =
+        AccessTools.MethodDelegate<EverPossibleToWatchFromDelegate>(AccessTools.DeclaredMethod(typeof(WatchBuildingUtility), "EverPossibleToWatchFrom"));
+
+    public delegate bool EverPossibleToWatchFromDelegate(IntVec3 watchCell, IntVec3 buildingCenter, Map map, bool bedAllowed, ThingDef def);
+}
