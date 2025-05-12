6e59791a85aa86b3f1128923a6bc86371d3f7f3f
SokyranTheDragon <exploder1531@gmail.com>
2025-05-13 00:06:36 +0200

    Computer learning now plays sounds

--- Source/AOMoreFurniture/JobDriver/JobDriver_ComputerLearning.cs
+++ Source/AOMoreFurniture/JobDriver/JobDriver_ComputerLearning.cs
@@ -2,14 +2,28 @@
 using RimWorld;
 using Verse;
 using Verse.AI;
+using Verse.Sound;
 
 namespace VanillaFurnitureEC;
 
 public class JobDriver_ComputerLearning : JobDriver
 {
+    protected ExtendedSitFacingJoyDataExtension joyData;
+    protected int nextSoundTick;
+    
     public override bool TryMakePreToilReservations(bool errorOnFailed)
     {
         return pawn.Reserve(TargetA, job, errorOnFailed: errorOnFailed) && pawn.ReserveSittableOrSpot(TargetThingA.InteractionCell, job, errorOnFailed: errorOnFailed);
+    }
+
+    public override void Notify_Starting()
+    {
+        base.Notify_Starting();
+
+        joyData = TargetA.Thing.def.GetModExtension<ExtendedSitFacingJoyDataExtension>();
+        // Initial sound delay, this one is between 0 and max
+        if (joyData?.sound != null)
+            nextSoundTick = Find.TickManager.TicksGame + Rand.RangeInclusive(0, joyData.soundRefireDelay.max);
     }
 
     protected override IEnumerable<Toil> MakeNewToils()
@@ -29,9 +43,26 @@ public class JobDriver_ComputerLearning : JobDriver
             pawn.rotationTracker.FaceTarget(TargetA);
             LearningUtility.LearningTickCheckEnd(pawn);
             pawn.skills.Learn(SkillDefOf.Intellectual, VFE_DefOf.VFE_ComputerLearning.xpPerTick);
+
+            if (joyData is { sound: not null } && Find.TickManager.TicksGame >= nextSoundTick)
+            {
+                nextSoundTick = Find.TickManager.TicksGame + joyData.soundRefireDelay.RandomInRange;
+                joyData.sound.PlayOneShot(new TargetInfo(TargetThingA));
+            }
         };
         computerUseToil.handlingFacing = true;
         computerUseToil.defaultCompleteMode = ToilCompleteMode.Never;
         yield return computerUseToil;
+    }
+
+    public override void ExposeData()
+    {
+        if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            joyData = TargetA.Thing.def.GetModExtension<ExtendedSitFacingJoyDataExtension>();
+
+        // Call to base needs to be done after we've done our PostLoadInit
+        base.ExposeData();
+
+        Scribe_Values.Look(ref nextSoundTick, nameof(nextSoundTick));
     }
 }
